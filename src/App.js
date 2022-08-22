5055dea16e77ce58422929efd47dfce70d758764
Simone Lungarella <simonelungarella@gmail.com>
2022-08-22 13:46:40 +0200

    Reactivated scroll bar

--- src/App.js
+++ src/App.js
@@ -224,7 +224,14 @@ const App = () => {
             backgroundColor: '#98acdc'
           },
           '*::-webkit-scrollbar': {
-            width: '0em'
+            width: '0.4em'
+          },
+          '*::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,.1)',
+            outline: '1px solid slategrey'
           }
         }}
       />
