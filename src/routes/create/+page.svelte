2f376dad2cbf20baf1f8a3c081c94818425d4f1c
Simone Lungarella <simonelungarella@gmail.com>
2023-01-18 19:37:26 +0100

    Test api call in static module

---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { slide } from "svelte/transition";
+  import axios from "axios";
+
+  const executeCall = async () => {
+    const response = await axios.get(
+      "https://dockyard-handler.herokuapp.com/v1.0.0/events"
+    );
+    console.log(response);
+  };
+</script>
+
+<div transition:slide={{ duration: 200 }} class="md:mt-16">
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    on:click={executeCall}>Execute</button
+  >
+</div>
