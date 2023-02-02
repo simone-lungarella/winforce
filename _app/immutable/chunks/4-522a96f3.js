346dc99c934b8dbfa54fc555f8624f43a92d1312
simone-lungarella <simone-lungarella@users.noreply.github.com>
2023-02-02 11:32:04 +0000

    deploy: c13ded0f72a9c36d863ac6d62126b1cdbf70a5fe

--- _app/immutable/chunks/4-522a96f3.js
+++ _app/immutable/chunks/4-522a96f3.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/windfarms/_page.svelte-0db58a00.js";export{t as component,r as universal};
+import{_ as r}from"./_page-90a7f2f4.js";import{default as t}from"../components/pages/windfarms/_page.svelte-0db58a00.js";export{t as component,r as universal};
